64247761b86b968ab10984d3c1304698705ddaa9
shreejan <shr.shreejan@gmail.com>
2026-04-14 19:18:02 +0930

    Basic player movement

---
+++ scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var movement_speed : float = 200
+var player_direction : Vector2
+
+func _physics_process(delta: float) -> void:
+    player_direction.x = Input.get_axis("move_left", "move_right")
+    player_direction.y = Input.get_axis("move_up", "move_down")
+    player_direction = player_direction.normalized()
+
+    if player_direction:
+        velocity = player_direction * movement_speed
+    else:
+        velocity = Vector2.ZERO
+
+    move_and_slide()
